ea0e472252c164198bbcd56284c647afbd884098
lespuch-v <36127590+lespuch-v@users.noreply.github.com>
2021-12-01 21:40:14 +0100

    initial commit

---
+++ scripts.js
@@ -0,0 +1,32 @@
+const timeEl = document.getElementById("countdown-display");
+
+const countDownDate = new Date("Dec 25, 2021 00:00:00").getTime();
+
+// Update the count down every 1 second
+const myInterval = setInterval(function () {
+  // Get today's date and time
+  const now = new Date().getTime();
+
+  // Find the minus between now and the count down date
+  const minus = countDownDate - now;
+
+  // Time calculations for days, hours, minutes and seconds
+  const days = Math.floor(minus / (1000 * 60 * 60 * 24));
+  const hours = Math.floor(
+    (minus % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+  );
+  const minutes = Math.floor((minus % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((minus % (1000 * 60)) / 1000);
+
+  // Display the result in the element with id="demo"
+  timeEl.innerHTML =
+    days + "days " + hours + "h " + minutes + " min " + seconds + "s ";
+
+  // If the count down is finished, write some text
+  if (minus < 0) {
+    clearInterval(myInterval);
+    timeEl.innerHTML = "Christmas!!!";
+  }
+}, 1000);
+
+
